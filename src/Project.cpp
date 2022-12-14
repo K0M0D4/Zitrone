899b0d0ca6430b52575fa0da9d939867e22c46bd
K0M0D4 <comoto45@gmail.com>
2022-12-14 19:35:19 +0000

    fixed note adding checks

--- src/Project.cpp
+++ src/Project.cpp
@@ -11,12 +11,22 @@ namespace cmt {
 
     void Project::addNote() {
         sf::Vector2i al{m_grid.getActiveLines()};
+        bool pass{false};
+
         if(m_notes.size() > 0) {
-            if(m_notes.back().getCoords().y < al.y) {
+            for(auto note : m_notes) {
+                if(note.getCoords().y != al.y) {
+                    pass = true;
+                } else {
+                    pass = false;
+                    break;
+                }
+            }
+            if(m_notes.back().getCoords().y < al.y && pass) {
                 m_notes.emplace_back(Note{sf::Vector2f{
                     al.x * m_breakBetweenNotesV + m_firstNoteOffset,
                     (al.y + 1) * m_breakBetweenNotesH}, al});
-            } else {
+            } else if(pass) {
                 for(uint16_t i{}; i < m_notes.size(); ++i) {
                     if(m_notes.at(i).getCoords().y < al.y) {
                         m_notes.emplace(m_notes.begin() + i,

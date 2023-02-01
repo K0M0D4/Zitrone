86bba55f9c68c5184f26e0db5a033ce9d3b8c9b3
K0M0D4 <comoto45@gmail.com>
2023-02-01 17:21:00 +0000

    clean main function

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,4 @@
 int main(int argc, char** argv) {
     cmt::App app;
     return app.start();
-
-    /*NFD_Init();
-
-    nfdchar_t* savePath{};
-
-    nfdfilteritem_t filterItem[1] {{"Project files", "ztp"}};
-
-    nfdresult_t result{NFD_SaveDialog(&savePath, filterItem, 1, NULL, "project.ztp")};
-    if(result == NFD_OKAY) {
-        std::cout << "okay\n";
-        NFD_FreePath(savePath);
-    } else if(result == NFD_CANCEL) {
-        std::cout << "cancel\n";
-    } else {
-        std::cout << "Error: " << NFD_GetError() << '\n';
-    }
-
-    NFD_Quit();*/
 }
